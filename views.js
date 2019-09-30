bdd489c410755eea91f49d3eee6dda00564cef33
zottwickel <scott.g.mckell@gmail.com>
2019-09-29 19:13:56 -0600

    add styles

--- views.js
+++ views.js
@@ -4,10 +4,10 @@ function setUp() {
             <section id="call">
                 <h1>Karaoke Time</h1>
                 <form id="search">
-                    <label for="song">Song name:</label><br>
-                    <input id="song" type="text" required><br>
-                    <label for="artist">Artist name:</label><br>
-                    <input id="artist" type="text" required><br>
+                    <label for="song">Song name</label><br>
+                    <input class="input" id="song" type="text" required><br>
+                    <label for="artist">Artist name</label><br>
+                    <input class="input" id="artist" type="text" required><br>
                     <input id="submit" type="submit" value="Search">
                 </form>
             </section>
@@ -18,7 +18,7 @@ function setUp() {
                 </div>
             </section>
         </main>
-    `
+    `;
 }
 
 function lyrics(lyricObject) {
@@ -30,11 +30,12 @@ function lyrics(lyricObject) {
         <div class="text">
             <p>${lyricObject.lyrics}</p>
         </div>
-    `
+    `;
 }
 
 function video(videoId) {
-    return `
-        <iframe width="250" height="140.625" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    `
+    if (window.screen.width >= 630) {
+        return `<iframe id="player" width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+    }
+    return `<iframe id="player" width="250" height="140.625" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 }
